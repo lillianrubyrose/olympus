58820876eed7d88c3acf15810797c1acfcbb510f
opusbopus <opusbopus@proton.me>
2024-03-09 18:10:11 -0500

    Add rustfmt

--- src/olympus-lexer/src/main.rs
+++ src/olympus-lexer/src/main.rs
@@ -2,29 +2,25 @@ use ariadne::{sources, Color, Label, Report};
 use olympus_lexer::lexer::{Lexer, Token};
 
 fn main() {
-    let filename = "example.olympus";
-    let src = include_str!("example.olympus");
-    let mut lexer = Lexer::new(src);
-    if let Err(err) = lexer.lex() {
-        Report::build(ariadne::ReportKind::Error, filename, err.span.start)
-            .with_message(err.value.clone())
-            .with_label(
-                Label::new((filename, err.span))
-                    .with_message(err.value)
-                    .with_color(Color::Red),
-            )
-            .finish()
-            .print(sources([(filename, src)]))
-            .unwrap();
-        return;
-    }
+	let filename = "example.olympus";
+	let src = include_str!("example.olympus");
+	let mut lexer = Lexer::new(src);
+	if let Err(err) = lexer.lex() {
+		Report::build(ariadne::ReportKind::Error, filename, err.span.start)
+			.with_message(err.value.clone())
+			.with_label(
+				Label::new((filename, err.span))
+					.with_message(err.value)
+					.with_color(Color::Red),
+			)
+			.finish()
+			.print(sources([(filename, src)]))
+			.unwrap();
+		return;
+	}
 
-    println!(
-        "{:?}",
-        lexer
-            .tokens
-            .into_iter()
-            .map(|v| v.value)
-            .collect::<Vec<Token>>()
-    );
+	println!(
+		"{:?}",
+		lexer.tokens.into_iter().map(|v| v.value).collect::<Vec<Token>>()
+	);
 }
--- src/olympus-parser/src/main.rs
+++ src/olympus-parser/src/main.rs
@@ -3,42 +3,38 @@ use olympus_lexer::lexer::{Lexer, SpannedErr};
 use olympus_parser::Parser;
 
 fn print_err<T>(src: &str, res: Result<T, SpannedErr>) -> bool {
-    if let Err(err) = res {
-        Report::build(
-            ariadne::ReportKind::Error,
-            "example.olympus",
-            err.span.start,
-        )
-        .with_message(err.value.clone())
-        .with_label(
-            Label::new(("example.olympus", err.span))
-                .with_message(err.value)
-                .with_color(Color::Red),
-        )
-        .finish()
-        .print(sources([("example.olympus", src)]))
-        .unwrap();
-        return true;
-    }
-    false
+	if let Err(err) = res {
+		Report::build(ariadne::ReportKind::Error, "example.olympus", err.span.start)
+			.with_message(err.value.clone())
+			.with_label(
+				Label::new(("example.olympus", err.span))
+					.with_message(err.value)
+					.with_color(Color::Red),
+			)
+			.finish()
+			.print(sources([("example.olympus", src)]))
+			.unwrap();
+		return true;
+	}
+	false
 }
 
 fn main() {
-    let src = include_str!("example.olympus");
-    let mut lexer = Lexer::new(src);
-    let lexer_err = print_err(src, lexer.lex());
-    if lexer_err {
-        println!("exited with lexer err");
-        return;
-    }
+	let src = include_str!("example.olympus");
+	let mut lexer = Lexer::new(src);
+	let lexer_err = print_err(src, lexer.lex());
+	if lexer_err {
+		println!("exited with lexer err");
+		return;
+	}
 
-    let mut parser = Parser::new(lexer.tokens);
-    let parser_err = print_err(src, parser.parse());
-    if parser_err {
-        println!("exited with parser err");
-        return;
-    }
+	let mut parser = Parser::new(lexer.tokens);
+	let parser_err = print_err(src, parser.parse());
+	if parser_err {
+		println!("exited with parser err");
+		return;
+	}
 
-    // dbg!(parser.enums);
-    dbg!(parser.servers);
+	// dbg!(parser.enums);
+	dbg!(parser.servers);
 }
