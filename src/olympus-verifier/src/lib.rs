f021d5decffb638abbda877d24bb1d9b0bc44044
opusbopus <opusbopus@proton.me>
2024-03-10 18:20:01 -0400

    (verifier): search for duplicate proc names, proc param names, and struct field names

--- src/olympus-verifier/src/lib.rs
+++ src/olympus-verifier/src/lib.rs
@@ -1,7 +1,10 @@
 use std::collections::HashMap;
 
 use olympus_common::{ErrorColor, OlympusError, Spanned};
-use olympus_parser::{ParsedEnum, ParsedEnumVariant, Parser};
+use olympus_parser::{
+	ParsedEnum, ParsedEnumVariant, ParsedProcedure, ParsedProcedureParam, ParsedRpcContainer, ParsedStruct,
+	ParsedStructField, Parser,
+};
 
 fn find_enum_variant_duplicates(variants: &[ParsedEnumVariant]) -> Result<(), OlympusError> {
 	let mut idents = HashMap::<String, (Spanned<String>, Option<Spanned<String>>)>::new();
@@ -40,6 +43,72 @@ fn find_enum_variant_duplicates(variants: &[ParsedEnumVariant]) -> Result<(), Ol
 	Ok(())
 }
 
+fn find_struct_field_duplicates(fields: &[ParsedStructField]) -> Result<(), OlympusError> {
+	let mut idents = HashMap::<String, (Spanned<String>, Option<Spanned<String>>)>::new();
+
+	for field in fields {
+		if let Some((_, duplicated)) = idents.get_mut(&field.ident.value) {
+			*duplicated = Some(field.ident.clone());
+		} else {
+			idents.insert(field.ident.value.clone(), (field.ident.clone(), None));
+		}
+	}
+
+	for (_, (original_ident, dup_ident)) in idents {
+		if let Some(dup_ident) = dup_ident {
+			return Err(OlympusError::new("Duplicate field ident found")
+				.label("Original here", original_ident.span, ErrorColor::Yellow)
+				.label("Duplicate here", dup_ident.span, ErrorColor::Red));
+		}
+	}
+
+	Ok(())
+}
+
+fn find_rpc_procedure_duplicates(procs: &[ParsedProcedure]) -> Result<(), OlympusError> {
+	let mut idents = HashMap::<String, (Spanned<String>, Option<Spanned<String>>)>::new();
+
+	for proc in procs {
+		if let Some((_, duplicated)) = idents.get_mut(&proc.ident.value) {
+			*duplicated = Some(proc.ident.clone());
+		} else {
+			idents.insert(proc.ident.value.clone(), (proc.ident.clone(), None));
+		}
+	}
+
+	for (_, (original_ident, dup_ident)) in idents {
+		if let Some(dup_ident) = dup_ident {
+			return Err(OlympusError::new("Duplicate proc ident found")
+				.label("Original here", original_ident.span, ErrorColor::Yellow)
+				.label("Duplicate here", dup_ident.span, ErrorColor::Red));
+		}
+	}
+
+	Ok(())
+}
+
+fn find_rpc_procedure_param_duplicates(params: &[ParsedProcedureParam]) -> Result<(), OlympusError> {
+	let mut idents = HashMap::<String, (Spanned<String>, Option<Spanned<String>>)>::new();
+
+	for param in params {
+		if let Some((_, duplicated)) = idents.get_mut(&param.ident.value) {
+			*duplicated = Some(param.ident.clone());
+		} else {
+			idents.insert(param.ident.value.clone(), (param.ident.clone(), None));
+		}
+	}
+
+	for (_, (original_ident, dup_ident)) in idents {
+		if let Some(dup_ident) = dup_ident {
+			return Err(OlympusError::new("Duplicate proc param ident found")
+				.label("Original here", original_ident.span, ErrorColor::Yellow)
+				.label("Duplicate here", dup_ident.span, ErrorColor::Red));
+		}
+	}
+
+	Ok(())
+}
+
 pub fn verify_parser_outputs(
 	Parser {
 		enums: parsed_enums,
@@ -48,8 +117,19 @@ pub fn verify_parser_outputs(
 		..
 	}: Parser,
 ) -> Result<(), OlympusError> {
-	for ParsedEnum { ident, variants } in parsed_enums {
-		find_enum_variant_duplicates(&variants)?;
+	for ParsedEnum { variants, .. } in &parsed_enums {
+		find_enum_variant_duplicates(variants)?;
+	}
+
+	for ParsedStruct { fields, .. } in &parsed_structs {
+		find_struct_field_duplicates(fields)?;
+	}
+
+	for ParsedRpcContainer { procedures } in &parsed_rpc_containers {
+		find_rpc_procedure_duplicates(procedures)?;
+		for proc in procedures {
+			find_rpc_procedure_param_duplicates(&proc.params)?;
+		}
 	}
 
 	Ok(())
