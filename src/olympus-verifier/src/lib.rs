2dee249a9e9582144c74a5759e57355f68b1472a
opusbopus <opusbopus@proton.me>
2024-03-10 16:22:44 -0400

    verify no duplicated enum variant idents

--- src/olympus-verifier/src/lib.rs
+++ src/olympus-verifier/src/lib.rs
@@ -1,29 +1,43 @@
 use std::collections::HashMap;
 
-use olympus_common::{OlympusError, Spanned};
+use olympus_common::{ErrorColor, OlympusError, Spanned};
 use olympus_parser::{ParsedEnum, ParsedEnumVariant, Parser};
 
 fn find_enum_variant_duplicates(variants: &[ParsedEnumVariant]) -> Result<(), OlympusError> {
-	variants
-		.iter()
-		.fold(HashMap::<i16, Option<Spanned<String>>, _>::new(), |mut acc, variant| {
-			if let Some(duplicated) = acc.get_mut(&variant.value) {
-				*duplicated = Some(variant.ident.clone());
-			} else {
-				acc.insert(variant.value, None);
-			}
-			acc
-		})
-		.into_iter()
-		.try_fold((), |acc, (_, ident)| {
-			if let Some(ident) = ident {
-				return Err(OlympusError::error(
-					"Enum with duplicated value found",
-					ident.span.clone(),
-				));
-			}
-			Ok(acc)
-		})
+	let mut idents = HashMap::<String, (Spanned<String>, Option<Spanned<String>>)>::new();
+	let mut values = HashMap::<i16, (Spanned<String>, Option<Spanned<String>>)>::new();
+
+	for variant in variants {
+		if let Some((_, duplicated)) = idents.get_mut(&variant.ident.value) {
+			*duplicated = Some(variant.ident.clone());
+		} else {
+			idents.insert(variant.ident.value.clone(), (variant.ident.clone(), None));
+		}
+
+		if let Some((_, duplicated)) = values.get_mut(&variant.value) {
+			*duplicated = Some(variant.ident.clone());
+		} else {
+			values.insert(variant.value, (variant.ident.clone(), None));
+		}
+	}
+
+	for (_, (original_ident, dup_ident)) in idents {
+		if let Some(dup_ident) = dup_ident {
+			return Err(OlympusError::new("Duplicate variant ident found")
+				.label("Original here", original_ident.span, ErrorColor::Yellow)
+				.label("Duplicate here", dup_ident.span, ErrorColor::Red));
+		}
+	}
+
+	for (_, (original_ident, dup_ident)) in values {
+		if let Some(dup_ident) = dup_ident {
+			return Err(OlympusError::new("Duplicate variant value found")
+				.label("Original here", original_ident.span, ErrorColor::Yellow)
+				.label("Duplicate here", dup_ident.span, ErrorColor::Red));
+		}
+	}
+
+	Ok(())
 }
 
 pub fn verify_parser_outputs(
