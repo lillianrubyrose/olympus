c785f3d5d237baecba6616ba03a00eb365495fc1
opusbopus <opusbopus@proton.me>
2024-03-17 05:17:05 -0400

    (net-common): Add more varint test cases

--- src/olympus-net-common/src/varint.rs
+++ src/olympus-net-common/src/varint.rs
@@ -100,21 +100,81 @@ impl_for_signed!(8, 16, 32, 64, 128);
 
 #[cfg(test)]
 mod tests {
+	use std::fmt::Debug;
+
 	use bytes::BytesMut;
 
-	use crate::varint::{read_varint_i64, read_varint_u64, write_varint_i64, write_varint_u64};
+	use crate::{
+		read_varint_i128, read_varint_i32, read_varint_i64, read_varint_i8, read_varint_u128, read_varint_u16,
+		read_varint_u32, read_varint_u64, read_varint_u8, write_varint_i128, write_varint_i32, write_varint_i64,
+		write_varint_i8, write_varint_u128, write_varint_u16, write_varint_u32, write_varint_u64, write_varint_u8,
+	};
 
-	#[test]
-	fn u64() {
+	fn test_write_read<T: Debug + PartialEq + Copy>(
+		value: T,
+		write_fn: fn(T, &mut BytesMut),
+		read_fn: fn(&mut BytesMut) -> T,
+	) {
 		let mut out = BytesMut::new();
-		write_varint_u64(u64::MAX, &mut out);
-		assert_eq!(u64::MAX, read_varint_u64(&mut out));
+		write_fn(value, &mut out);
+		assert_eq!(value, read_fn(&mut out));
 	}
 
 	#[test]
-	fn i64() {
-		let mut out = BytesMut::new();
-		write_varint_i64(i64::MIN, &mut out);
-		assert_eq!(i64::MIN, read_varint_i64(&mut out));
+	fn test_u8() {
+		test_write_read(0u8, write_varint_u8, read_varint_u8);
+		test_write_read(u8::MAX, write_varint_u8, read_varint_u8);
+	}
+
+	#[test]
+	fn test_i8() {
+		test_write_read(i8::MIN, write_varint_i8, read_varint_i8);
+		test_write_read(0i8, write_varint_i8, read_varint_i8);
+		test_write_read(i8::MAX, write_varint_i8, read_varint_i8);
+	}
+
+	#[test]
+	fn test_u16() {
+		test_write_read(0u16, write_varint_u16, read_varint_u16);
+		test_write_read(u16::MAX, write_varint_u16, read_varint_u16);
+	}
+
+	#[test]
+	fn test_u32() {
+		test_write_read(0u32, write_varint_u32, read_varint_u32);
+		test_write_read(u32::MAX, write_varint_u32, read_varint_u32);
+	}
+
+	#[test]
+	fn test_i32() {
+		test_write_read(i32::MIN, write_varint_i32, read_varint_i32);
+		test_write_read(0i32, write_varint_i32, read_varint_i32);
+		test_write_read(i32::MAX, write_varint_i32, read_varint_i32);
+	}
+
+	#[test]
+	fn test_u64() {
+		test_write_read(0u64, write_varint_u64, read_varint_u64);
+		test_write_read(u64::MAX, write_varint_u64, read_varint_u64);
+	}
+
+	#[test]
+	fn test_i64() {
+		test_write_read(i64::MIN, write_varint_i64, read_varint_i64);
+		test_write_read(0i64, write_varint_i64, read_varint_i64);
+		test_write_read(i64::MAX, write_varint_i64, read_varint_i64);
+	}
+
+	#[test]
+	fn test_u128() {
+		test_write_read(0u128, write_varint_u128, read_varint_u128);
+		test_write_read(u128::MAX, write_varint_u128, read_varint_u128);
+	}
+
+	#[test]
+	fn test_i128() {
+		test_write_read(i128::MIN, write_varint_i128, read_varint_i128);
+		test_write_read(0i128, write_varint_i128, read_varint_i128);
+		test_write_read(i128::MAX, write_varint_i128, read_varint_i128);
 	}
 }
